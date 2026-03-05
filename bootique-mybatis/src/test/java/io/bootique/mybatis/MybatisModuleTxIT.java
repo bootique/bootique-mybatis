2884850415a647d973a8c6b7d1067cc090c75ed0
Andrus Adamchik <andrus@objectstyle.com>
2026-03-05 16:36:39 -0500

    upgrading to module-parents 4.0.5

--- bootique-mybatis/src/test/java/io/bootique/mybatis/MybatisModuleTxIT.java
+++ bootique-mybatis/src/test/java/io/bootique/mybatis/MybatisModuleTxIT.java
@@ -20,10 +20,10 @@ package io.bootique.mybatis;
 
 import io.bootique.BQRuntime;
 import io.bootique.Bootique;
-import io.bootique.jdbc.junit5.derby.DerbyTester;
-import io.bootique.junit5.BQApp;
-import io.bootique.junit5.BQTest;
-import io.bootique.junit5.BQTestTool;
+import io.bootique.jdbc.junit.derby.DerbyTester;
+import io.bootique.junit.BQApp;
+import io.bootique.junit.BQTest;
+import io.bootique.junit.BQTestTool;
 import io.bootique.mybatis.testmappers2.T2Mapper;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionManager;
